55a41f19b34fa1a0691f02b0dd16cb23b5fe0d08
joel vilca Alania <joevil23tjq@gmail.com>
2020-12-12 15:00:48 -0500

    test

--- app/recursion.js
+++ app/recursion.js
@@ -2,15 +2,39 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
-
+    let fs = fs || require('fs'),
+    files = fs.readdirSync(dir);
+    filelist = filelist || [];
+    files.forEach(function(file) {
+      if (fs.statSync(dir + '/' + file).isDirectory()) {
+        filelist = this.listFiles(dir + '/' + file, filelist);
+      }
+      else {
+        filelist.push(file);
+      }
+    });
+    return filelist;
   },
 
   permute: function(arr) {
-
+    let result = [];  if (arr.length === 0) return [];
+    if (arr.length === 1) return [arr];
+    for (let i = 0; i < arr.length; i++) {
+      const currentNum = arr[i];
+      const remainingarr = arr.slice(0, i).concat(arr.slice(i + 1));
+      const remainingarrPermuted = this.permute(remainingarr);
+      for (let j = 0; j < remainingarrPermuted.length; j++) {
+        const permutedArray = [currentNum].concat(remainingarrPermuted[j]);
+        result.push(permutedArray);
+      }
+    }
+    return result;
   },
 
   fibonacci: function(n) {
-
+    return n <1 ? 0
+    : n<=2 ? 1
+    : this.fibonacci(n-1) + this.fibonacci(n-2)
   },
 
   validParentheses: function(n) {
--- app/regex.js
+++ app/regex.js
@@ -2,15 +2,19 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+    return /\d/.test(str);
   },
 
   containsRepeatingLetter: function(str) {
-
+    const strArr = str.toLowerCase().split("").sort().join("").match(/(.)\1+/g);
+    if(strArr.length) return true
+    return strArr.length
   },
 
   endsWithVowel: function(str) {
-
+    let r = /^([aeiou]).*\1$/i
+    console.log(r.test(str))
+    return r.test(str)
   },
 
   captureThreeNumbers: function(str) {
@@ -22,6 +26,7 @@ exports.regexAnswers = {
   },
 
   isUSD: function(str) {
-
+    let r = /^\$(0|[1-9][0-9]{0,2})(,\d{3})*(\.\d{1,2})?$/
+    return r.test(str)
   }
 };
